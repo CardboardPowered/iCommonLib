84d8389e6bba8f6699446ed76aa11cb62f880815
IsaiahPatton <isaiahpatton1@gmail.com>
2023-06-10 16:20:32 -0400

    minor cleanup

--- iCommon-API/src/main/java/me/isaiah/common/IRegistry.java
+++ iCommon-API/src/main/java/me/isaiah/common/IRegistry.java
@@ -1,17 +1,4 @@
 package me.isaiah.common;
 
-import net.minecraft.entity.effect.StatusEffect;
-import net.minecraft.util.registry.Registry;
-
-public class IRegistry {
-
-	public void test() {
-		Registry<StatusEffect> effects = Registry.STATUS_EFFECT;
-		
-		for (StatusEffect e : effects) {
-			
-		}
-		
-	}
-	
+public class IRegistry {	
 }
