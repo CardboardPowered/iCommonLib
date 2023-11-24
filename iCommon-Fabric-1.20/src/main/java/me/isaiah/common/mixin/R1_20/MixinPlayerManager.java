33181467f9bfe56d8715bf0d64efaf025697d20c
Thosea / JF <104597976+ItsThosea@users.noreply.github.com>
2023-11-24 00:42:18 -0500

    Use SyncedClientOption.createDefault

--- iCommon-Fabric-1.20/src/main/java/me/isaiah/common/mixin/R1_20/MixinPlayerManager.java
+++ iCommon-Fabric-1.20/src/main/java/me/isaiah/common/mixin/R1_20/MixinPlayerManager.java
@@ -2,13 +2,11 @@ package me.isaiah.common.mixin.R1_20;
 
 import com.mojang.authlib.GameProfile;
 import me.isaiah.common.cmixin.IMixinPlayerManager;
-import net.minecraft.network.message.ChatVisibility;
 import net.minecraft.network.packet.c2s.common.SyncedClientOptions;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.PlayerManager;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
-import net.minecraft.util.Arm;
 import org.spongepowered.asm.mixin.Mixin;
 
 @Mixin(PlayerManager.class)
@@ -16,7 +14,7 @@ public class MixinPlayerManager implements IMixinPlayerManager {
 
     @Override
     public ServerPlayerEntity InewPlayer(MinecraftServer server, ServerWorld world, GameProfile profile) {	
-        return new ServerPlayerEntity(server, world, profile, new SyncedClientOptions("en-us", 8, ChatVisibility.FULL, true, Byte.MAX_VALUE, Arm.RIGHT, false, true));
+        return new ServerPlayerEntity(server, world, profile, SyncedClientOptions.createDefault());
     }
 
 }
