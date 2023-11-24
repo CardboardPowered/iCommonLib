eaa227dca11321312933a7687634f2f8659c72d7
Thosea / JF <104597976+ItsThosea@users.noreply.github.com>
2023-11-23 22:08:40 -0500

    1.20.2

--- iCommon-Fabric-1.20/src/main/java/me/isaiah/common/mixin/R1_20/MixinPlayerManager.java
+++ iCommon-Fabric-1.20/src/main/java/me/isaiah/common/mixin/R1_20/MixinPlayerManager.java
@@ -1,24 +1,22 @@
 package me.isaiah.common.mixin.R1_20;
 
-import org.spongepowered.asm.mixin.Mixin;
-
 import com.mojang.authlib.GameProfile;
-
 import me.isaiah.common.cmixin.IMixinPlayerManager;
-import net.minecraft.network.encryption.PlayerPublicKey;
-import net.minecraft.network.encryption.SignatureVerifier;
+import net.minecraft.network.message.ChatVisibility;
+import net.minecraft.network.packet.c2s.common.SyncedClientOptions;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.PlayerManager;
-import net.minecraft.server.network.ServerLoginNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.Arm;
+import org.spongepowered.asm.mixin.Mixin;
 
 @Mixin(PlayerManager.class)
 public class MixinPlayerManager implements IMixinPlayerManager {
 
     @Override
     public ServerPlayerEntity InewPlayer(MinecraftServer server, ServerWorld world, GameProfile profile) {	
-        return new ServerPlayerEntity(server, world, profile);
+        return new ServerPlayerEntity(server, world, profile, new SyncedClientOptions("en-us", 8, ChatVisibility.FULL, true, Byte.MAX_VALUE, Arm.RIGHT, false, true));
     }
 
 }
